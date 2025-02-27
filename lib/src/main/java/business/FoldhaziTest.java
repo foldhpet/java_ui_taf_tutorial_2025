8a26dbe18593c8dca4802d61189e77df90a31a01
Péter Földházi Jr <10651180+foldhpet@users.noreply.github.com>
2025-02-27 16:54:45 -0600

    Create singleton Driver class

--- lib/src/main/java/business/FoldhaziTest.java
+++ lib/src/main/java/business/FoldhaziTest.java
@@ -1,6 +1,7 @@
 package business;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
 import core.BaseTest;
 
--- lib/src/main/java/core/BaseTest.java
+++ lib/src/main/java/core/BaseTest.java
@@ -1,19 +1,19 @@
 package core;
 
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
 public class BaseTest {
-    protected ChromeDriver driver;
+    protected WebDriver driver;
 
     @BeforeEach
     public void Setup() {
-        driver = new ChromeDriver();
+        driver = Driver.getDriver();
     }
 
     @AfterEach
     public void TearDown() {
-        driver.quit();
+        Driver.closeDriver();
     }
 }
